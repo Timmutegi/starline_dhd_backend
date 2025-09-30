f41322aaacfff415f3445d71f8fd5f09b2896e9c
Timmutegi <mutegi.timothy538@gmail.com>
2025-09-30 20:46:43 +0300

    Fix CORS issue

--- app/main.py
+++ app/main.py
@@ -42,10 +42,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # Add audit middleware for HIPAA compliance
